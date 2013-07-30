d3747634cffae5e11f1b6f50e175e9da0c8cad55
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-30 00:16:37 -0400

    Track sort works with all anticipated inputs

--- zipfs_song/spec/zipf_spec.rb
+++ zipfs_song/spec/zipf_spec.rb
@@ -35,4 +35,30 @@ describe Zipfer do
   it "sorts the list" do
     expect(zipfer.best_tracks).to eql(["four", "two", "three", "one"])
   end
+
+  let(:tracklist2) {[
+      [197812, "re_hash"],
+      [78906, "5_4"],
+      [189518, "tomorrow_comes_today"],
+      [39453, "new_genious"],
+      [210492, "clint_eastwood"],
+      [26302, "man_research"],
+      [22544, "punk"],
+      [19727, "sound_check"],
+      [17535, "double_bass"],
+      [18782, "rock_the_house"],
+      [198189, "19_2000"],
+      [13151, "latin_simone"],
+      [12139, "starshine"],
+      [11272, "slow_country"],
+      [10521, "m1_a1"]
+    ]}
+
+    let(:zipfer2) { Zipfer.new(tracklist2) }
+
+    it "sorts the list" do
+      expect(zipfer2.best_tracks).to eql(["19_2000", "clint_eastwood", "tomorrow_comes_today",
+        "re_hash", "rock_the_house", "sound_check", "double_bass", "m1_a1", "new_genious", 
+        "man_research", "latin_simone", "5_4", "punk", "slow_country", "starshine"])
+    end
 end
