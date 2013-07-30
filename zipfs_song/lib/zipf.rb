1007378d348c4a0d6d0fff41567467fc2d20d104
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-30 00:13:29 -0400

    Implement track sort by quality

--- zipfs_song/lib/zipf.rb
+++ zipfs_song/lib/zipf.rb
@@ -5,20 +5,35 @@ class Zipfer < Hash
   def initialize(tracklist)
     index = 1
     tracklist.each do |track| 
-      self[track[1]] = [track[0], index]
+      self[track[1]] = Track.new(track[1], track[0], index)
       index += 1
     end
   end
 
-  def f(track)
-    self[track][0]
+  def best_tracks
+    self.sort_by { |title, track| track.q }.reverse!.map { |array| array[0] }
+  end
+end
+
+class Track
+
+  attr_accessor :title, :play_count, :track_number
+
+  def initialize(title, play_count, track_number)
+    @title = title
+    @play_count = play_count
+    @track_number = track_number
   end
 
-  def z(track)
-    1.0 / self[track][1]
+  def f
+    @play_count
   end
 
-  def q(track)
-    f(track) / z(track)
+  def z
+    1.0 / @track_number
+  end
+
+  def q
+    (f / z).to_i
   end
 end
--- zipfs_song/spec/zipf_spec.rb
+++ zipfs_song/spec/zipf_spec.rb
@@ -12,23 +12,27 @@ describe Zipfer do
   let(:zipfer) { Zipfer.new(tracklist) }
 
   it "computes the f value for a song" do
-    expect(zipfer.f("one")).to eql(30)
-    expect(zipfer.f("two")).to eql(30)
-    expect(zipfer.f("three")).to eql(15)
-    expect(zipfer.f("four")).to eql(25)
+    expect(zipfer["one"].f).to eql(30)
+    expect(zipfer["two"].f).to eql(30)
+    expect(zipfer["three"].f).to eql(15)
+    expect(zipfer["four"].f).to eql(25)
   end
 
   it "computes the z value for a song" do
-    expect(zipfer.z("one")).to eql(1.0)
-    expect(zipfer.z("two")).to eql(0.5)
-    expect(zipfer.z("three")).to eql(0.3333333333333333)
-    expect(zipfer.z("four")).to eql(0.25)
+    expect(zipfer["one"].z).to eql(1.0)
+    expect(zipfer["two"].z).to eql(0.5)
+    expect(zipfer["three"].z).to eql(0.3333333333333333)
+    expect(zipfer["four"].z).to eql(0.25)
   end
 
   it "computes the q value for a song" do
-    expect(zipfer.q("one")).to eql(30.0)
-    expect(zipfer.q("two")).to eql(60.0)
-    expect(zipfer.q("three")).to eql(45.0)
-    expect(zipfer.q("four")).to eql(100.0)
+    expect(zipfer["one"].q).to eql(30)
+    expect(zipfer["two"].q).to eql(60)
+    expect(zipfer["three"].q).to eql(45)
+    expect(zipfer["four"].q).to eql(100)
+  end
+
+  it "sorts the list" do
+    expect(zipfer.best_tracks).to eql(["four", "two", "three", "one"])
   end
 end
