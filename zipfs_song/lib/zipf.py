ed755eaa61b2d00da5bbe0db98302591b5d7ec65
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-30 02:48:34 -0400

    Add Python version of zipfs_song

--- zipfs_song/lib/zipf.py
+++ zipfs_song/lib/zipf.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python
 
-import sys
 from __future__ import division
+import sys
+import re
 
-# while 1:
-#     try:
-#         line = sys.stdin.readline()
-#     except KeyboardInterrupt:
-#         break
+class Zipfer:
 
-#     if not line:
-#         break
+    def __init__(self, tracklist):
+        self.index = 1
+        self.tracklist = {}
+        for track in tracklist:
+            self.tracklist[track[1]] = Track(track[1], int(track[0]), self.index)
+            self.index += 1
 
-#     print binary_reverser(line)
+    def best_tracks(self, how_many):
+        return [(x[0]) for x in sorted(self.tracklist.items(), key=lambda x:x[1].q())][::-1][0:how_many]
 
-# class Zipfer:
-
-#     def __init__(self, tracklist):
-#         index = 1
-#         self.tracklist = {}
-#         for track in tracklist:
-#             self[track[1]] = Track(track[1], track[0], index)
-#             index += 1
-
-#     def best_tracks:
-#         tracklist.sort_by { |title, track| track.q }.reverse!.map { |array| array[0] }
 
 class Track:
 
   def __init__(self, title, play_count, track_number):
     self.title = title
-    self.play_count = play_counter
+    self.play_count = play_count
     self.track_number = track_number
 
   def f(self):
@@ -41,3 +32,19 @@ class Track:
 
   def q(self):
     return self.f()/self.z()
+
+def line_reader():
+    first_line = sys.stdin.readline()
+    if not first_line:
+        return
+    inputs = re.findall(r'\d+', first_line)
+    n = int(inputs[0])
+    m = int(inputs[1])
+    tracklist = []
+    for num in range(n):
+        tracklist.append(sys.stdin.readline().split())
+    z = Zipfer(tracklist)
+    print z.best_tracks(m)
+    line_reader()
+
+line_reader()
